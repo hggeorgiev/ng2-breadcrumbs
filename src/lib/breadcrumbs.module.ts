f158829144004b720888fc41230537ab2d17a640
Amiram Korach <amiramk@gmail.com>
2018-12-20 13:20:22 +0200

    upgrade to ng7

---
+++ src/lib/breadcrumbs.module.ts
@@ -0,0 +1,31 @@
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import {BreadcrumbComponent} from "./breadcrumbs.component";
+import {RouterModule} from "@angular/router";
+import {BrowserModule} from "@angular/platform-browser";
+import {CommonModule} from "@angular/common";
+import {BreadcrumbsService} from "./breadcrumbs.service";
+
+@NgModule({
+
+    declarations: [
+        BreadcrumbComponent
+    ],
+    providers: [
+        BreadcrumbsService
+    ],
+    imports: [
+        RouterModule,
+        BrowserModule,
+        CommonModule
+    ],
+    exports: [BreadcrumbComponent]
+})
+export class BreadcrumbsModule {
+    constructor() {}
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: BreadcrumbsModule,
+        };
+    }
+}
+
---
+++ src/lib/public_api.ts
@@ -0,0 +1,4 @@
+export * from './breadcrumbs.component';
+export * from './breadcrumbs.service';
+export * from './breadcrumbs.model';
+export * from './breadcrumbs.module';
